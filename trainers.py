5c7f6838848cdc909a3daa3b0551809a6b1a3320
yonghee12 <suncar1000@gmail.com>
2020-08-05 00:12:50 +0900

    added torch

--- trainers.py
+++ trainers.py
@@ -1,13 +1,22 @@
-from deepnp.models import *
-from progress_timer import Timer
+from itertools import chain
 from time import perf_counter as counter
+
+import torch
+
+from torch.nn import RNN, Linear
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.optim import SGD
+
+from deepnp.models import *
 
 
 class RNNTrainer:
-    def __init__(self, input_dim, hidden_dim, output_size):
+    def __init__(self, input_dim, hidden_dim, output_size, backend='np'):
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.output_size = output_size
+        self.backend = backend
 
         D, H, V = input_dim, hidden_dim, output_size
         self.rnn_Wx = np.random.randn(D, H) / np.sqrt(D)
@@ -45,7 +54,10 @@ class RNNTrainer:
         if self.total_size % self.batch_size_global != 0:
             self.max_iters += 1
 
-        return self.train(X, y_true, batch_size, lr, n_epochs, print_many, verbose)
+        if self.backend in ['np', 'numpy', 'cupy', 'cp']:
+            return self.train_np(X, y_true, batch_size, lr, n_epochs, print_many, verbose)
+        elif self.backend in ['torch', 'pytorch']:
+            return self.train_torch(X, y_true, batch_size, lr, n_epochs, print_many, verbose)
 
     def predict(self, x):
         if x.shape[1] != self.input_dim or x.ndim != 2:
@@ -63,7 +75,56 @@ class RNNTrainer:
         probs = softmax(fc_out)
         return np.argmax(probs, axis=1).item()
 
-    def train(self, X, y_true, batch_size, learning_rate, num_epochs, print_many, verbose):
+    def train_torch(self, X, y_true, batch_size, learning_rate, num_epochs, print_many, verbose):
+        self.batch_size_global = batch_size
+        progresses = {int(num_epochs // (100 / i)): i for i in range(1, 101, 1)}
+        t0 = counter()
+        durations = []
+
+        device = torch.device('cuda:0')
+        rnn = RNN(input_size=self.input_dim, hidden_size=self.hidden_dim, num_layers=1, nonlinearity='tanh',
+                  bias=True, batch_first=False).to(device)
+        fc = Linear(self.hidden_dim, self.output_size, bias=True).to(device)
+        params = [rnn.parameters(), fc.parameters()]
+        optimizer = SGD(chain(*params), lr=learning_rate)
+        for epoch in range(num_epochs):
+            epoch_loss = 0
+            for i in range(self.max_iters):
+                x_batch = X[i * self.batch_size_global:(i + 1) * self.batch_size_global]
+                x_batch = np.array([x_batch[:, step, :] for step in range(self.time_steps)])
+                y_true_batch = y_true[i * self.batch_size_global:(i + 1) * self.batch_size_global]
+                batch_size_local = x_batch.shape[1]
+
+                # convert to pytorch tensor
+                y_true_batch = y_true_batch.astype(np.int64)
+                y_true_batch = torch.tensor(y_true_batch, requires_grad=False).to(device)
+                x_batch = x_batch.astype(np.float32)
+                x_batch = torch.tensor(x_batch, requires_grad=True).to(device)
+
+                # forward pass
+                h_stack, h_last = rnn.forward(x_batch)
+                fc_out = fc.forward(h_last)
+                log_y_pred = F.log_softmax(input=fc_out, dim=2)
+                log_y_pred = log_y_pred.view(batch_size_local, self.output_size)
+                loss = F.nll_loss(input=log_y_pred, target=y_true_batch, reduction='mean')
+
+                # update gradient
+                optimizer.zero_grad()
+                loss.backward()
+                epoch_loss += loss.item()
+                optimizer.step()
+
+            durations.append(counter() - t0)
+            t0 = counter()
+            if (print_many and epoch % 100 == 0) or (not print_many and epoch in progresses):
+                print(f"after epoch: {epoch}, epoch_losses: {round(epoch_loss / self.max_iters, 3)}")
+
+        if verbose > 0:
+            avg_epoch_time = sum(durations) / len(durations)
+            print("average epoch time:", round(avg_epoch_time, 3))
+            return avg_epoch_time
+
+    def train_np(self, X, y_true, batch_size, learning_rate, num_epochs, print_many, verbose):
         self.batch_size_global = batch_size
         progresses = {int(num_epochs // (100 / i)): i for i in range(1, 101, 1)}
         t0 = counter()
@@ -86,11 +147,6 @@ class RNNTrainer:
                 fc_out = fc.forward(x=h_last)
                 loss_value = loss.forward(x=fc_out, y_true=y_true_batch)
                 epoch_losses.append(loss_value)
-
-                # if (print_many and epoch % 100 == 0) or (not print_many and epoch in progresses):
-                # print(f"epoch: {epoch}, loss: {round(loss_value, 4)}, y_true: {y_true_batch}")
-                # print(f"pred_prob: {softmax(fc_out)[np.arange(batch_size_local), y_true_batch]}")
-                # print('-' * 100)
 
                 # backward pass
                 d_L = loss.backward()
