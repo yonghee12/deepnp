15ae16781b5d9329b8090f7a585f69fc5dd1ce49
yonghee12 <suncar1000@gmail.com>
2020-08-13 04:13:41 +0900

    1) implemented update method in rnn layer 2) added FCLayerWithTimesteps and SoftmaxLossLayerWithTimestep

--- layers/recurrent.py
+++ layers/recurrent.py
@@ -63,6 +63,10 @@ class RNNLayer:
                       'bias_grad': np.zeros_like(bias)}
         self.timestep_cells = []
 
+    def update(self, parameters):
+        self.parameters = parameters
+        self.timestep_cells = list()
+
     def forward(self, x_sequence, h_init=None):
         batch_size, timesteps, input_dim = x_sequence.shape
         N, T, D, H = batch_size, timesteps, input_dim, self.hidden_dim
@@ -75,9 +79,8 @@ class RNNLayer:
             timestep_cell = RNNCell(N, D, H, Wx=Wx, Wh=Wh, bias=bias)
             h_next = timestep_cell.forward(x_sequence[:, t, :], h_prev)  # 해당 timestep마다의 (N, D) 형태 2차원 텐서, h_prev
             self.timestep_cells.append(timestep_cell)
-            h_stack[:, t, :] = h_next   # h_next는 (N, H)의 한 timestep rnn 결과물
+            h_stack[:, t, :] = h_next  # h_next는 (N, H)의 한 timestep rnn 결과물
             h_prev = h_next
-
 
         # T*N*D Style (Deprecated)
         # for time_step, x_t in enumerate(x_sequence):
@@ -89,18 +92,17 @@ class RNNLayer:
         h_last = h_next
         return h_last, h_stack
 
-    def backward(self, d_h=1):
-        _, Wx, Wh, bias = self.parameters
+    def backward(self, d_h=1, optimize=True):
+        Wx, Wh, bias = self.parameters
         d_Wx = np.zeros_like(Wx)
         d_Wh = np.zeros_like(Wh)
         d_bias = np.zeros_like(bias)
 
         for idx, layer in enumerate(reversed(self.timestep_cells)):
-            grad = layer.backward(d_h_next=d_h)
+            grad = layer.backward(d_h_next=d_h, optimize=optimize)
             d_Wx += grad['Wx_grad']
             d_Wh += grad['Wh_grad']
             d_bias += grad['bias_grad']
-            d_h = grad['h_prev_grad']
 
         for d in [d_Wx, d_Wh, d_bias]:
             np.clip(d, -1, 1, out=d)
@@ -108,6 +110,58 @@ class RNNLayer:
         self.grads['Wx_grad'][...] = d_Wx
         self.grads['Wh_grad'][...] = d_Wh
         self.grads['bias_grad'][...] = d_bias
-        self.grads["h_init_grad"][...] = d_h
 
         return self.grads
+
+
+class FCLayerTimesteps:
+    def __init__(self, W, bias):
+        self.params = [W, bias]
+        self.grads = {'W_grad': np.zeros_like(W),
+                      'bias_grad': np.zeros_like(bias)}
+        self.x = None
+
+    def forward(self, x):
+        W, bias = self.params
+        N, T, H = x.shape
+        assert H == W.shape[0]
+        self.x = x
+
+        x_flat = x.reshape(N * T, H)
+        out_flat = np.matmul(x_flat, W) + bias
+        out = out_flat.reshape(N, T, -1)
+        return out
+
+    def backward(self, dout):
+        W, bias = self.params
+        dx = np.matmul(dout, W.T)
+        dW = np.matmul(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads['W_grad'][...] = dW
+        self.grads['x_grad'][...] = dx
+        self.grads['bias_grad'][...] = db
+
+        return self.grads
+
+
+class SoftmaxWithLossLayerTimesteps:
+    def __init__(self):
+        self.y_pred = None
+        self.y_true = None
+
+    def forward(self, x, y_true):
+        """
+        :param x: (N, T, V) 3-dimensional
+        :param y_true: (N, T) 2-dimensional
+        :return:
+        """
+        N, T, V = x.shape
+        assert N == y_true.shape[0] and T == y_true.shape[1]
+        assert x.ndim == 3 and y_true.ndim == 2
+        x_flat = x.reshape(N * T, V)
+        y_true_flat = y_true.reshape(1, N * T)
+        y_pred = softmax(x_flat)
+        average_loss = cross_entropy_error(y_pred, y_true_flat)
+        self.y_pred, self.y_true = y_pred, y_true
+        return average_loss
