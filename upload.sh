744d302d957e5f3365541a48a4d4b2814dab6033
yonghee12 <suncar1000@gmail.com>
2020-08-22 03:30:43 +0900

    upload to pypi

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-rm -r build dist direct_redis.egg-info
+#rm -r build dist deepnp.egg-info
 python setup.py sdist bdist_wheel \
 && twine upload dist/*
-rm -r build dist direct_redis.egg-info
+rm -r build dist deepnp.egg-info
